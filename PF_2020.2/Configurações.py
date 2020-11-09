31a36751208dd4fcdfa6c017d75fd8a55a2c2fe1
Pedro Drumond <drumondpe@gmail.com>
2020-11-09 11:17:13 -0300

    Começando as configurações
    Co-authored-by: Luiz Felipe Borelli Durand <luizfbd@al.insper.edu.br>

--- PF_2020.2/Configurações.py
+++ PF_2020.2/Configurações.py
@@ -2,3 +2,36 @@
 
 #Configurações
 
+import math
+import pygame
+vetor = pygame.math.Vector2
+
+class Configurações():
+    #define as configurações básicas
+    def __init__(self):
+        self.titulo = 'Star Fight'
+
+        self.largura_tela = 600
+        self.altura_tela = 670
+
+class Cores():
+    #define as cores básicas
+    def __init__(self):
+        self.vermelho = (255, 0, 0)
+        self.preto = (0, 0, 0)
+        self.branco = (255, 255, 255)
+        self.aqua = (0, 128, 128)
+        self.azul_marinho = (0, 0, 128)
+        self.verde = (0, 255, 0)
+        self.laranja = (255, 165, 0)
+        self.amarelo = (255, 255, 0)
+
+class Textos(): 
+    #define tamanho e fonte dos textos
+    def __init__(self):
+
+        self.fonte = 'Futura ZBlk BT'
+        self.tamanho_grande = 56
+        self.tamanho_pequeno = 40
+
+
--- PF_2020.2/Primeiro código.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-#Primeiro teste
-
-#primeiro código
