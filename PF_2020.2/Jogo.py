83788df3347683af42313d880d1c86514d4f9071
Pedro Drumond <drumondpe@gmail.com>
2020-11-11 07:39:52 -0300

    começando o codigo do jogo

--- PF_2020.2/Jogo.py
+++ PF_2020.2/Jogo.py
@@ -2,4 +2,18 @@
 """
 Autores: Luiz Durand, Henrry Miguel e Pedro Drumond
 """
-#Jogo
+import pygame
+
+from Configurações import Config
+
+CONFIG = Config()
+TEXTOS = Textos()
+
+def rodar_jogo():
+    #inicializa o jogo
+    pygame.init()
+    pygame.mixer.init()
+
+    tela = pygame.display.set_mode((CONFIG.largura_tela, CONFIG.altura_tela))
+    relogio = pygame.time.Clock()
+    
