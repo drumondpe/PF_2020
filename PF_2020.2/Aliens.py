5f34508b5d820fd1a72b5f6a41a6543f50b7678c
Pedro Drumond <drumondpe@gmail.com>
2020-11-16 11:19:36 -0300

    Começando o player

--- PF_2020.2/Aliens.py
+++ PF_2020.2/Aliens.py
@@ -19,10 +19,33 @@ class Alien(pygame.sprite.Sprite):
         ALTURA_alien = 30
         self.imagem = pygame.image.load('Alien.png')convert_alpha() #carrega a imagem
         #self.imagem = pygame.transform.scale(self.imagem, (23, 23)) #muda o tamanho da imagem
-        self.imagem_diminui = pygame.transform.scale(imagem, (LARGURA_alien, ALTURA_alien))
+        self.imagem_diminui = pygame.transform.scale(self.imagemimagem, (LARGURA_alien, ALTURA_alien))
         self.imagem.set_colorkey((CORES.preto))
 
         self.alien_x = 100
         self.alien_y = -ALTURA_alien
         self.alien_velocidadeX = 3
         self.alien_velocidadeY = 1
+
+
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self):
+        self.image = pygame.image.load('assets/img/meteorBrown_med1.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (METEOR_WIDTH, METEOR_HEIGHT))
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, WIDTH-METEOR_WIDTH)
+        self.rect.y = random.randint(-100, -METEOR_HEIGHT)
+        self.speedx = random.randint(-3, 3)
+        self.speedy = random.randint(2, 9)
+
+    def update(self):
+        # Atualizando a posição do meteoro
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        # Se o meteoro passar do final da tela, volta para cima e sorteia
+        # novas posições e velocidades
+        if self.rect.top > HEIGHT or self.rect.right < 0 or self.rect.left > WIDTH:
+            self.rect.x = random.randint(0, WIDTH-METEOR_WIDTH)
+            self.rect.y = random.randint(-100, -METEOR_HEIGHT)
+            self.speedx = random.randint(-3, 3)
+            self.speedy = random.randint(2, 9)
--- PF_2020.2/Player.py
+++ PF_2020.2/Player.py
@@ -9,3 +9,29 @@ CONFIG = Config()
 TEXTOS = Textos()
 CORES = Cores()
 
+class Nave(pygame.sprite.Sprite):
+    #classe do player
+
+    def __init__(self, tela, CONFIG):
+
+        self.tela = tela
+        self.CONFIG = CONFIG
+        self.NAVE_largura = 30
+        self.NAVE_altura = 20
+
+        self.imagem = pygame.image.load('Nave.png').convert_alpha()
+        self.imagem = pygame.transform.scale(self.imagem, (NAVE_largura, NAVE_altura))
+        self.rect = self.imagem.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.velocidadeX = 0
+    
+    def atualiza_posicao_NAVE(self):
+        #atualiza a posição da nave
+        self.rect.x += self.velocidadeX
+
+        #Mantem dentro da tela
+        if self.rect.right > CONFIG.largura_tela:
+            self.rect.right = CONFIG.largura_tela
+        if self.rect.left < 0:
+            self.rect.left = 0
